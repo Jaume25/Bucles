414935a8033bdae92285d1f89a66a3e1421d3ed1
ism47968960 <ism47968960@a02.informatica.escoladeltreball.org>
2018-02-08 13:22:10 +0100

    08-2-18

--- mayork08.py
+++ mayork08.py
@@ -5,11 +5,13 @@ x=int(input("Escriba un numero par: "))
 while x%2 != 0:
 	x=int(input("Escriba un numero par: "))
 resp=raw_input("Quiere escribir otro numero par? (S/N) ")
+
 while resp == "S" or resp == "s":
 	x=int(input("Escriba otro numero par: "))
 	while x%2 != 0:
 		x=int(input("Escriba otro numero par: "))
 	resp=raw_input("Quiere escribir otro numero par? (S/N) ")
+print "Programa terminado"
 
 	
 				
